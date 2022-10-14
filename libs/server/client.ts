99c1c51555d920cba38cf19bc787cf406d30583e
KirschX <mk44879@gmail.com>
2022-10-14 21:28:38 +0900

    fixed client config

--- libs/server/client.ts
+++ libs/server/client.ts
@@ -3,6 +3,7 @@ import { PrismaClient } from "@prisma/client";
 declare global {
   var client: PrismaClient | undefined;
 }
+
 declare global {
   var prisma: PrismaClient | undefined;
 }
@@ -10,5 +11,6 @@ declare global {
 const client = global.client || new PrismaClient();
 
 if (process.env.NODE_ENV === "development") global.client = client;
-else if (process.env.NODE_ENV !== "production") globalThis.prisma = client;
-export default client;
+if (process.env.NODE_ENV !== "production") globalThis.prisma = client;
+
+export default new PrismaClient();
