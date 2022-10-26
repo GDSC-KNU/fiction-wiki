d9449ad1eb13cf5bbaae9fd641ad61e30ef8934b
KirschX <mk44879@gmail.com>
2022-10-26 12:47:49 +0900

    deleted dummy array on fiction params

--- components/headMeata.tsx
+++ components/headMeata.tsx
@@ -5,7 +5,7 @@ const HeadMeta = ({ title, description, url }: any) => {
     <Head>
       <title>{title || "FDBS"}</title>
       <meta
-        name="description"
+        name="og:description"
         content={description || "웹소설 DB 제공 서비스"}
       />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />\
@@ -15,7 +15,7 @@ const HeadMeta = ({ title, description, url }: any) => {
         property="og:url"
         content={url || "https://fdbs-proto.vercel.app/"}
       />
-      <meta property="og:image" content={`/public/fdb_logo.png`} />
+      <meta property="og:image" content={`%public%/fdb_logo.png`} />
       <meta property="og:article:author" content="article.author" />
     </Head>
   );
