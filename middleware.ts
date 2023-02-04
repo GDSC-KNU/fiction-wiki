62ea12e9ce147f0201cd9b9fb549515d7183e4c4
KirschX <mk44879@gmail.com>
2023-02-04 21:34:40 +0900

    fix: middleware redirection

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,6 @@
 import type { NextFetchEvent, NextRequest } from "next/server";
 import { NextResponse } from "next/server";
+// import { withAuth } from "next-auth/middleware";
 
 export function middleware(req: NextRequest, ev: NextFetchEvent) {
   const requestForNextAuth = {
@@ -8,17 +9,7 @@ export function middleware(req: NextRequest, ev: NextFetchEvent) {
     },
   };
 
-  // console.log(req.cookies.get("next-auth.csrf-token"));
-  // console.log(requestForNextAuth.headers.cookie);
-  // console.log(req.cookies.get("next-auth.csrf-token"));
-  // console.log("hi");
-  // const session = await getSession({ req: requestForNextAuth });
-  // const ua = userAgent(req);
-  // if (ua?.isBot) {
-  //   return NextResponse.next();
-  // }
-  // if (req.nextUrl.pathname.startsWith("/enter")) {
-  // }
+  // console.log(req.cookies.get("next-auth.session-token"));
 
   if (
     req.nextUrl.pathname.startsWith("/fictions/create") &&
@@ -36,8 +27,27 @@ export function middleware(req: NextRequest, ev: NextFetchEvent) {
 
   if (
     req.nextUrl.pathname.startsWith("/profile") &&
-    !req.cookies.get("next-auth.csrf-token")
+    !req.cookies.get("next-auth.session-token")
   ) {
     return NextResponse.rewrite(new URL("/enter", req.url));
   }
 }
+
+/////
+
+// export default withAuth(
+//   // `withAuth` augments your `Request` with the user's token.
+
+//   function middleware(req) {
+//     // console.log(req.nextauth.token);
+//     console.log("hey");
+//     console.log(req.nextauth.token);
+//   },
+//   {
+//     callbacks: {
+//       authorized: ({ token }) => (token ? true : false),
+//     },
+//   }
+// );
+
+// export const config = { matcher: ["/profile", "/fictions/create"] };
