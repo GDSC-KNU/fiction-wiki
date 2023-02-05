533779ab2f309160a05abdbf772805933024b892
KirschX <mk44879@gmail.com>
2023-02-05 17:31:10 +0900

    chore: added enter page instruction

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -60,7 +60,9 @@ const Enter: NextPage = () => {
 
   return (
     <div className="mt-16 px-4">
-      <h3 className="text-3xl font-bold text-center">Enter to FDBS</h3>
+      <h3 className="text-3xl font-bold text-center">
+        Enter to FDBS(현재 구글 로그인만 가능)
+      </h3>
       <div className="mt-12">
         {data?.ok ? (
           <form
