8a12109b006611aa3e4fa4992524151ef1a54466
KirschX <mk44879@gmail.com>
2023-07-19 17:41:19 +0900

    fix: refactored wikiNavBar

--- src/components/fiction/wikiNavBar.tsx
+++ src/components/fiction/wikiNavBar.tsx
@@ -1,30 +1,41 @@
 import { FictionContext } from "@src/context/fictionContext";
+
 import Link from "next/link";
-import { useContext } from "react";
+import { useRouter } from "next/router";
 
 const WikiNavBar = () => {
-  let fictionContext = useContext(FictionContext);
+  const router = useRouter();
+  const { id: fictionId } = router.query;
+
+  const routes = [
+    { id: "read", name: "읽기", path: `/fictions/${fictionId}` },
+    { id: "edit", name: "편집", path: `/fictions/${fictionId}/edit` },
+    { id: "history", name: "히스토리", path: `/fictions/${fictionId}/history` },
+    { id: "share", name: "공유", path: `/fictions/${fictionId}/share` },
+  ];
+
+  const activeClass = (route: any) => {
+    if (router.pathname === "/fictions/[id]" && route === "read")
+      return " font-mono text-2xl font-bold text-blue-600";
+
+    const isActive = router.pathname.includes(route);
+    return isActive ? " font-mono text-2xl font-bold text-blue-600" : "";
+  };
 
   return (
     <ul className=" flex items-center justify-center space-x-2 whitespace-nowrap border-b-[1px] border-r-[1px] py-2 uppercase lg:ml-2 lg:block lg:flex-col lg:space-x-0 lg:space-y-4 lg:border-b-0 lg:border-r-0 ">
-      <li className=" cursor-pointer ">
-        <Link className=" font-mono text-2xl font-bold " href="/" passHref>
-          READ
-        </Link>
-      </li>
-      <li className=" ">
-        <Link href={`/fictions/${fictionContext.fiction?.id}/edit`}>Edit</Link>
-      </li>
-      <li className="">
-        <Link href={`/fictions/${fictionContext.fiction?.id}/history`} passHref>
-          History
-        </Link>
-      </li>
-      <li className="">
-        <Link href={`/fictions/${fictionContext.fiction?.id}/share`} passHref>
-          Share
-        </Link>
-      </li>
+      {routes.map(({ id, name, path }) => (
+        <li
+          key={id}
+          className={`cursor-pointer font-bold text-gray-800 ${activeClass(
+            id
+          )}`}
+        >
+          <Link href={path} passHref>
+            {name}
+          </Link>
+        </li>
+      ))}
     </ul>
   );
 };
