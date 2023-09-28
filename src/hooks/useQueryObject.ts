d8c31e2cff7fd5a01e045e4d5a2ffb8b34bd8ca6
KirschX <mk44879@gmail.com>
2023-09-28 18:54:01 +0900

    feat: added quetyObject atom and useQueryObject hook

---
+++ src/hooks/useQueryObject.ts
@@ -0,0 +1,40 @@
+import { useRecoilState, useRecoilValue } from "recoil";
+import { queryObjectAtom, queryStringSelector } from "@store/atoms";
+
+export function useQueryObject() {
+  const [queryObject, setQueryObject] = useRecoilState(queryObjectAtom);
+  const queryString = useRecoilValue(queryStringSelector);
+
+  const updateArr = (key: string, value: string, isChecked: boolean) => {
+    const currentValues = queryObject[key as "keywords" | "categories"] || [];
+    return isChecked
+      ? [...currentValues, value]
+      : currentValues.filter((item: string) => item !== value);
+  };
+
+  const updateQueryObject = (target: EventTarget & HTMLInputElement) => {
+    const { value, checked: isChecked, name } = target;
+
+    const updatedQueryObject = {
+      ...queryObject,
+      page: 1, // assuming you always want to reset the page on any update
+      [name]: ["keywords", "categories"].includes(name)
+        ? updateArr(name, value, isChecked)
+        : isChecked
+        ? value
+        : "",
+    };
+
+    setQueryObject(updatedQueryObject);
+  };
+
+  const updatePage = (value: number) =>
+    setQueryObject({ ...queryObject, page: value });
+
+  return {
+    queryObject,
+    queryString,
+    updateQueryObject,
+    updatePage,
+  };
+}
--- src/store/atoms.ts
+++ src/store/atoms.ts
@@ -1,6 +1,69 @@
 "use client";
 
 import { atom, selector } from "recoil";
+import qs from "query-string";
+import { QS_PARSE_OPTIONS, QS_STRINGIFY_OPTIONS } from "@/utils/qsOption";
+
+const SetQueryObjectAtomEffect = ({ onSet }: any) => {
+  onSet((newValue: any) => {
+    const { pathname } = location;
+    const newUrl = qs.stringify(newValue, QS_STRINGIFY_OPTIONS as any);
+    const as = `${pathname}${newUrl ? "?" : ""}${newUrl}`;
+    window.history.replaceState(null, "", as);
+  });
+};
+
+interface IInitialQueryObject {
+  keywords: string[];
+  nationalities: string;
+  categories: string[];
+  sorting: string;
+  dateYear: string;
+  page: number;
+}
+
+const initialQueryObject = (): IInitialQueryObject => {
+  const defaultQueryObject: IInitialQueryObject = {
+    keywords: [],
+    nationalities: "",
+    categories: [],
+    sorting: "",
+    dateYear: "",
+    page: 1,
+  };
+
+  if (!globalThis.location) return defaultQueryObject;
+
+  const search = globalThis.location.search;
+  const parsed = qs.parse(search, QS_PARSE_OPTIONS as any);
+
+  return {
+    keywords: Array.isArray(parsed.keywords) ? parsed.keywords : ([] as any),
+    nationalities:
+      typeof parsed.nationalities === "string" ? parsed.nationalities : "",
+    categories: Array.isArray(parsed.categories)
+      ? parsed.categories
+      : ([] as any),
+    sorting: typeof parsed.sorting === "string" ? parsed.sorting : "",
+    dateYear: typeof parsed.dateYear === "string" ? parsed.dateYear : "",
+    page: typeof parsed.page === "string" ? parseInt(parsed.page) : 1,
+  };
+};
+
+export const queryObjectAtom = atom({
+  key: "queryObject",
+  default: initialQueryObject(),
+  effects: [SetQueryObjectAtomEffect],
+});
+
+export const queryStringSelector = selector({
+  key: "queryString",
+  get: ({ get }) => {
+    const queryObject = get(queryObjectAtom);
+    const queryString = qs.stringify(queryObject, QS_STRINGIFY_OPTIONS as any);
+    return queryString;
+  },
+});
 
 export const fictionPageAtom = atom({
   key: "whatPage",
@@ -15,30 +78,4 @@ export const authorPageAtom = atom({
 export const searchPageAtom = atom({
   key: "searchWhatPage",
   default: 1,
-});
-
-export const queryObjectAtom = atom({
-  key: "queryObject",
-  default: {
-    keywords: [],
-    nationalities: "all",
-    categories: [],
-    sorting: "all",
-    releaseTimeFilter: "all",
-    dateYear: "all",
-    page: 1,
-  },
-});
-
-export const queryStringSelector = selector({
-  key: "queryString",
-  get: ({ get }) => {
-    const queryObject = get(queryObjectAtom);
-    return Object.entries(queryObject)
-      .map(
-        ([key, value]) =>
-          `${key}=${Array.isArray(value) ? value.join(",") : value}`
-      )
-      .join("&");
-  },
 });
