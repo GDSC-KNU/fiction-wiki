daa4e7602e0dfef14af1843ab250fc81f5ccd796
KirschX <mk44879@gmail.com>
2023-12-10 20:46:49 +0900

    chore(metatag): added siteName and applicationName

--- src/app/fictions/page.tsx
+++ src/app/fictions/page.tsx
@@ -27,10 +27,12 @@ export default async function FictionsPage() {
 }
 
 export const metadata: Metadata = {
+  applicationName: "소설위키",
   title: ` 작품 | 소설위키`,
   description: "원하는 소설에 대한 정보를 직접 검색해서 찾아보세요.",
   keywords: `작품목록, 작품검색, 소설목록, 소설검색`,
   openGraph: {
+    siteName: "소설위키",
     type: "website",
     title: ` 작품 | 소설위키`,
     description: "원하는 소설에 대한 정보를 직접 검색해서 찾아보세요.",
