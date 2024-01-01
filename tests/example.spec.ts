f8a95d25cdaa445cf1f90c74c839468cfa987a83
KirschX <68739701+KirschX@users.noreply.github.com>
2024-01-01 20:18:55 +0900

    Github actions로 CI/CD 파이프라인 테스트 (#6)
    * fix: updated readme
    
    * feat: added playwrite.yml
    
    * fix: updated paywright.yml
    
    * fix: updated playwright config
    
    * chore
    
    * chore
    
    * chore
    
    * fix: added env variables
    
    * fix: updated github actions flow
    
    * fix: updated BASE_URL

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,12 +1,14 @@
 import { test, expect } from "@playwright/test";
 
+const BASE_URL = process.env.BASE_URL || "http://localhost:3000";
+
 test("should navigate to the about page", async ({ page }) => {
   // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
-  await page.goto("http://localhost:3000");
+  await page.goto(BASE_URL);
   // Find an element with the text 'About' and click on it
   await page.click("text=소설위키");
   // The new URL should be "/about" (baseURL is used there)
-  await expect(page).toHaveURL("http://localhost:3000");
+  await expect(page).toHaveURL(BASE_URL);
   // // The new page should contain an h1 with "About"
   // await expect(page.locator("h1")).toContainText("About");
   // The new page should contain the text "소설위키"
